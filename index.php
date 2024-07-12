f74b6130a784b786349820c31f60654028fbdd21
Hamzah Waffa <hamzahwaffa10@gmail.com>
2024-07-12 15:30:20 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae minus repudiandae quos perferendis nisi eligendi illo quia ipsam esse, harum reiciendis suscipit, nulla, repellendus incidunt voluptates dolorum qui magni. Quisquam.</p>
 </body>
 </html>
