deef7e7ec4627a565c8a974427f728198b02f9ad
Hamzah Waffa <hamzahwaffa10@gmail.com>
2024-07-12 15:46:40 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae minus repudiandae quos perferendis nisi eligendi illo quia ipsam esse, harum reiciendis suscipit, nulla, repellendus incidunt voluptates dolorum qui magni. Quisquam.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
